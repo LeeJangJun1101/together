9cdca882cadc18c95db10f1bc045e1e61a27c1eb
kch07261 <kch07261@naver.com>
2023-06-28 11:40:30 +0900

    Kch07261Servlet 추가!

---
+++ src/main/java/com/together/Kch07261Servlet.java
@@ -0,0 +1,24 @@
+package com.together;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Kch07261Servlet
+ */
+@WebServlet("/kch07261")
+public class Kch07261Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Hello, kch07261");
+	}
+
+}
